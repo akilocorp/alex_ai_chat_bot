cdc151ddddce6e76dcd89ffdd8aaf26980235a6d
Yonathan Kidanemariam <150378613+akilocorp@users.noreply.github.com>
2025-06-13 23:21:02 +0300

    removed sidebar and implemented params

--- test_app.py
+++ test_app.py
@@ -1,5 +1,8 @@
-# test_app.py
-import streamlit as st
-
-print("Hello from test_app.py!") # This should definitely print
-st.write("Hello, Streamlit!")
+import os
+persist_dir = "./alex_characteristics"
+if os.path.exists(persist_dir):
+    print(f"DEBUG: Found existing directory '{persist_dir}'. Deleting contents for a clean recreation.")
+   
+else:
+    print(f"DEBUG: Directory '{persist_dir}' does not exist. Will create it.")
+    os.makedirs(persist_dir, exist_ok=True) # Ensure parent directory exists if it's nested
